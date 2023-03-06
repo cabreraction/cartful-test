6688ae8ecbdaab63dca268a81d8e60d85911318b
Javier Cabrera <cabreraction@gmail.com>
2023-03-06 10:16:46 -0600

    first commit for challenge 3

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # 1st commit for challenge 1
 
 # 1st commit for challenge 2
+
+# 1st commit for challenge 3
---
+++ challenge-3/questionaire.js
@@ -0,0 +1,34 @@
+module.exports = [
+  {
+    questionId: 1,
+    questionName: 'What is you favorite color?',
+    answers: [
+      {
+        answerName: 'Red',
+        answerId: 11
+      },
+      {
+        answerName: 'Blue',
+        answerId: 12
+      },
+      {
+        answerName: 'Pink',
+        answerId: 13
+      }
+    ]
+  },
+  {
+    questionId: 2,
+    questionName: 'Are you a flat-earther?',
+    answers: [
+      {
+        answerName: 'Yes',
+        answerId: 21
+      },
+      {
+        answerName: 'No',
+        answerId: 22
+      }
+    ]
+  }
+]
